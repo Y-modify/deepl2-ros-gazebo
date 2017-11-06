0a8dd6fecbd241affb3ce051c70e45a91b5ac062
coord.e <me@coord-e.com>
2017-11-07 00:14:05 +0900

    Format python script using autopep8

--- src/yamax_learn/scripts/yamaxenv.py
+++ src/yamax_learn/scripts/yamaxenv.py
@@ -33,30 +33,38 @@ import rl.callbacks
 
 #config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 #session = tf.Session(config=config)
-#tensorflow_backend.set_session(session)
+# tensorflow_backend.set_session(session)
+
 
 class Clipper(Processor):
     def process_action(self, action):
         return np.clip(action, -1., 1.)
 
+
 class YamaXSimEnv(gazebo_env.GazeboEnv):
     def __init__(self):
-        gazebo_env.GazeboEnv.__init__(self, "/yamax/src/yamax_gazebo/launch/world.launch")
-	self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
-	self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        gazebo_env.GazeboEnv.__init__(
+            self, "/yamax/src/yamax_gazebo/launch/world.launch")
+        self.reset_proxy = rospy.ServiceProxy(
+            '/gazebo/reset_simulation', Empty)
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
         self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
-        self.joints = ["neck", "shoulder_right_x", "shoulder_right_z", "shoulder_left_x", "shoulder_left_z", "elbow_right", "elbow_left", "backbone_1", "backbone_2", "hip_joint_right_z", "hip_joint_right_x", "hip_joint_left_z", "hip_joint_left_x", "knee_right", "knee_left", "ankle_1_right", "ankle_1_left", "ankle_2_right", "ankle_2_left"]
+        self.joints = ["neck", "shoulder_right_x", "shoulder_right_z", "shoulder_left_x", "shoulder_left_z", "elbow_right", "elbow_left", "backbone_1", "backbone_2",
+                       "hip_joint_right_z", "hip_joint_right_x", "hip_joint_left_z", "hip_joint_left_x", "knee_right", "knee_left", "ankle_1_right", "ankle_1_left", "ankle_2_right", "ankle_2_left"]
         self.fail_threshold = 0.5
         self.success_threshold = 5
         self.dt = 0.1
-        self.publishers = map(lambda j: rospy.Publisher('/yamax/'+j+'_position_controller', Float64, queue_size=10), self.joints)
+        self.publishers = map(lambda j: rospy.Publisher(
+            '/yamax/' + j + '_position_controller', Float64, queue_size=10), self.joints)
 
-	high_joints = np.array([1] * len(self.joints))
+        high_joints = np.array([1] * len(self.joints))
         # 行動空間 (+, 0, -の関節数乗) 1をもっと細かくするもありかも
         self.action_space = gym.spaces.Box(low=-high_joints, high=high_joints)
 
-        high = np.array([1.57] * len(self.joints) + [1] + [10]) # 観測空間(state)の次元 (関節数+加速度+位置) とそれらの最大値(1)
-        self.observation_space = gym.spaces.Box(low=-high, high=high) # 最小値は、最大値のマイナスがけ
+        # 観測空間(state)の次元 (関節数+加速度+位置) とそれらの最大値(1)
+        high = np.array([1.57] * len(self.joints) + [1] + [10])
+        self.observation_space = gym.spaces.Box(
+            low=-high, high=high)  # 最小値は、最大値のマイナスがけ
 
     def n2a(self, x, n):
         sum = []
@@ -76,14 +84,16 @@ class YamaXSimEnv(gazebo_env.GazeboEnv):
         acc = None
         while acc is None:
             try:
-                acc = rospy.wait_for_message('/yamax/imu', Imu, timeout = 5).linear_acceleration.x
+                acc = rospy.wait_for_message(
+                    '/yamax/imu', Imu, timeout=5).linear_acceleration.x
             except:
                 pass
 
         pos = None
         while pos is None:
             try:
-                pos = rospy.wait_for_message('/yamax/odom', Odometry, timeout = 5).pose.pose.position.x
+                pos = rospy.wait_for_message(
+                    '/yamax/odom', Odometry, timeout=5).pose.pose.position.x
             except:
                 pass
 
@@ -113,7 +123,7 @@ class YamaXSimEnv(gazebo_env.GazeboEnv):
         # action = n2a(action_n, 3)
         # actionを受け取り、次のstateを決定
         for (i, act) in enumerate(action):
-            diff = (act - 1) * self.dt # acc: -0.1 0 0.1
+            diff = (act - 1) * self.dt  # acc: -0.1 0 0.1
             state = self.current_state[i] + diff
             self.current_state[i] = np.clip(state, -1.57, 1.57)
             self.publishers[i].publish(self.current_state[i])
@@ -141,16 +151,16 @@ class YamaXSimEnv(gazebo_env.GazeboEnv):
     # 各episodeの開始時に呼ばれ、初期stateを返すように実装
     def _reset(self):
 
-        rospy.wait_for_service('/gazebo/reset_simulation') # reset env
+        rospy.wait_for_service('/gazebo/reset_simulation')  # reset env
         try:
-            #reset_proxy.call()
+            # reset_proxy.call()
             self.reset_proxy()
         except (rospy.ServiceException) as e:
             print ("/gazebo/reset_simulation service call failed")
 
         self.current_state = [0] * len(self.joints)
         for pub in self.publishers:
-            pub.publish(0) # reset degs
+            pub.publish(0)  # reset degs
 
         self.unpause_simulation()
 
@@ -159,6 +169,7 @@ class YamaXSimEnv(gazebo_env.GazeboEnv):
         self.pause_simulation()
 
         return np.array(self.current_state + [current_accel, current_position])
+
 
 env = YamaXSimEnv()
 np.random.seed(123)
@@ -177,7 +188,8 @@ actor.add(Activation('tanh'))
 print(actor.summary())
 
 action_input = Input(shape=(nb_actions,), name='action_input')
-observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
+observation_input = Input(
+    shape=(1,) + env.observation_space.shape, name='observation_input')
 flattened_observation = Flatten()(observation_input)
 x = Dense(400)(flattened_observation)
 x = Activation('relu')(x)
@@ -192,7 +204,8 @@ print(critic.summary())
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
 memory = SequentialMemory(limit=100000, window_length=1)
-random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.1)
+random_process = OrnsteinUhlenbeckProcess(
+    size=nb_actions, theta=.15, mu=0., sigma=.1)
 agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
                   memory=memory, nb_steps_warmup_critic=1000, nb_steps_warmup_actor=1000,
                   random_process=random_process, gamma=.99, target_model_update=1e-3,
